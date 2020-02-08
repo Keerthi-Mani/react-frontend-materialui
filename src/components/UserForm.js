6c063cb44332138295aeb828566a39371cf7630c
Keerthi Mani <carkeane.getz@gmail.com>
2020-02-08 12:18:56 -0500

    next and prev state

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,6 +1,34 @@
 import React, { Component } from "react";
 
 class UserForm extends Component {
+  state = {
+    step: 1,
+    firstName: "",
+    lastName: "",
+    email: "",
+    occupation: "",
+    city: "",
+    bio: ""
+  };
+
+  // Proceed to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+
+  // Go back to previous step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  //Handle field change
+
   render() {
     return <div>Hello</div>;
   }
