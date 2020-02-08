1e9ecb799cc448a3e8b5db475a90c39d2740dbe3
Keerthi Mani <carkeane.getz@gmail.com>
2020-02-08 12:46:05 -0500

    import material ui for appbar,theme,button, text

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,11 +1,46 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
 
 class FormUserDetails extends Component {
   render() {
+    const { values, handleChange } = this.props;
+
     return (
-      <div>
-        <h1>FormUserDetails</h1>
-      </div>
+      <MuiThemeProvider>
+        <Fragment>
+          <AppBar title="Enter User Details" />
+
+          <TextField
+            placeholder="Enter Your First Name"
+            label="First Name"
+            onChange={handleChange("firstName")}
+            defaultValue={values.firstName}
+            margin="normal"
+            fullWidth="true"
+          />
+          <br />
+          <TextField
+            placeholder="Enter Your Last Name"
+            label="Last Name"
+            onChange={handleChange("lastName")}
+            defaultValue={values.lastName}
+            margin="normal"
+            fullWidth="true"
+          />
+          <br />
+          <TextField
+            placeholder="Enter Your First Name"
+            label="First Name"
+            onChange={handleChange("firstName")}
+            defaultValue={values.firstName}
+            margin="normal"
+            fullWidth="true"
+          />
+        </Fragment>
+      </MuiThemeProvider>
     );
   }
 }
