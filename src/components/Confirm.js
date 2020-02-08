96158026a859d78f852aa89961d6e8c7e81455ca
Keerthi Mani <carkeane.getz@gmail.com>
2020-02-08 15:30:56 -0500

    List and listitems are listed

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -1,13 +1,60 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import { List, ListItem } from "material-ui/List";
+import RaisedButton from "material-ui/RaisedButton";
 
 class Confirm extends Component {
+  continue = e => {
+    e.preventDefault();
+    //Process Form//
+    this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
   render() {
+    const {
+      values: { firstName, lastName, email, occupation, city, bio }
+    } = this.props;
+
     return (
-      <div>
-        <h1>Confirm</h1>
-      </div>
+      <MuiThemeProvider>
+        <Fragment>
+          <AppBar title="Confirm User Data" />
+          <List>
+            <ListItem primaryText="First Name" secondaryText={firstName} />
+            <ListItem primaryText="Last Name" secondaryText={lastName} />
+            <ListItem primaryText="Email" secondaryText={email} />
+            <ListItem primaryText="Occupation" secondaryText={occupation} />
+            <ListItem primaryText="City" secondaryText={city} />
+            <ListItem primaryText="Bio" secondaryText={bio} />
+          </List>
+          <br />
+          <RaisedButton
+            label="Back"
+            primary={false}
+            style={styles.button}
+            onClick={this.back}
+          />
+          <RaisedButton
+            label="Confirm & Continue"
+            primary={true}
+            onClick={this.continue}
+            style={styles.button}
+          />
+        </Fragment>
+      </MuiThemeProvider>
     );
   }
 }
 
+const styles = {
+  button: {
+    margin: 15
+  }
+};
 export default Confirm;
