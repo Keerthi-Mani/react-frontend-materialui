0e9055332056b0ec80f4365a5c6e8d5755a8dfe8
Keerthi Mani <carkeane.getz@gmail.com>
2020-02-08 15:38:09 -0500

    thank you for submission

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,13 +1,35 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
 
 class Success extends Component {
+  continue = e => {
+    e.preventDefault();
+    //Process Form//
+    this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
   render() {
     return (
-      <div>
-        <h1>Success</h1>
-      </div>
+      <MuiThemeProvider>
+        <Fragment>
+          <AppBar title="Success" />
+          <h1>Thank You For Your Submission</h1>
+          <p>You will get an email with further Instructions</p>
+        </Fragment>
+      </MuiThemeProvider>
     );
   }
 }
 
+const styles = {
+  button: {
+    margin: 15
+  }
+};
 export default Success;
