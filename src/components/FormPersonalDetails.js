44b92de2c356b7036a9cd4cb4baf3362ff5259ee
Keerthi Mani <carkeane.getz@gmail.com>
2020-02-08 14:21:33 -0500

    import material ui

--- src/components/FormPersonalDetails.js
+++ src/components/FormPersonalDetails.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
 
 class FormPersonalDetails extends Component {
   render() {
